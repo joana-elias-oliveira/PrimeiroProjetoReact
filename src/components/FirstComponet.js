840ba1299cdb93f3b500615bb982d34b353c3351
Joana Elias <joana.oliverelias@outlook.com>
2023-10-05 15:17:54 -0300

    joo

---
+++ src/components/FirstComponet.js
@@ -0,0 +1,13 @@
+function FirstComponent() {
+    //algum comentraio
+    const name = "jozinha";
+
+    return (
+        <div className="firstcomponent">
+            <p>Primeiro Componente</p>
+            <p>Nome: {name}</p>
+        </div>
+    );
+}
+
+export default FirstComponent
